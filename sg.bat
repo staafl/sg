19eb8ff895f80bad8592d69f4bc28f5fc9633169
Velko Nikolov <velko.nikolov@gmail.com>
2018-02-10 14:51:15 +0200

    3js

--- sg.bat
+++ sg.bat
@@ -1,3 +1,3 @@
-for %%a in (.) do set currentfolder=%%~na
+@for %%a in (.) do @set currentfolder=%%~na
 @start "" "http://trustingwolves.com/%currentfolder%/%1"
 ::start "" "https://htmlpreview.github.io/?https://github.com/staafl/threejs-demos/blob/master/%1/index.html"
--- ssh.bat
+++ ssh.bat
@@ -1,1 +1,4 @@
-call ssh.exe trusting@ams7.siteground.eu -p 18765
+@for %%a in (.) do @set currentfolder=%%~na
+::call ssh.exe trusting@ams7.siteground.eu  -p 18765
+@call ssh-key sg
+@call ssh.exe trusting@ams7.siteground.eu  -p 18765 -t  "cd /home/trusting/public_html/%currentfolder%; bash"
