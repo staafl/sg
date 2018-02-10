78a98bc79373a8f268bcf72db8d3e11fff0f9616
Velko Nikolov <velko.nikolov@gmail.com>
2018-02-10 15:02:12 +0200

    concat

--- _new/concat.bat
+++
@@ -1,3 +0,0 @@
-@del concat.js
-@touch concat.js
-@cat ..\_misc\ExplodeModifier.js >> concat.js
---
+++ concat.bat
@@ -0,0 +1,3 @@
+@del _misc\concat.js
+@touch _misc\concat.js
+@cat \_misc\ExplodeModifier.js >> _misc\concat.js
