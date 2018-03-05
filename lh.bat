c7294aaee57c683be95f9921aa9a48e99cc2923d
Velko Nikolov <velko.nikolov@gmail.com>
2018-03-05 23:26:10 +0200

    stuff

--- lh.bat
+++ lh.bat
@@ -1,5 +1,5 @@
-@call %~d0\\%~p0\\run-mongoose
+@call %~d0\\%~p0\\mong.bat
 ::@start "" chrome --incognito "http://localhost:3333/%1"
 @start "" "http://localhost:3333/%1"
-@start %~d0\\%~p0\\%1
+::@start %~d0\\%~p0\\%1
 ::start "" "https://htmlpreview.github.io/?https://github.com/staafl/threejs-demos/blob/master/%1/index.html"
