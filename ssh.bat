c46695e3cfca8bb934ac998a373b7c404f7b25dd
Velko Nikolov <velko.nikolov@tick42.com>
2018-03-07 19:20:48 +0200

    misc

--- ssh.bat
+++ ssh.bat
@@ -1,4 +1,4 @@
 @for %%a in (.) do @set currentfolder=%%~na
 ::call ssh.exe trusting@ams7.siteground.eu  -p 18765
 @call ssh-key sg
-@call ssh.exe trusting@ams7.siteground.eu  -p 18765 -t  "cd /home/trusting/public_html/%currentfolder%; bash"
+@call ssh-temp sg ssh.exe trusting@ams7.siteground.eu  -p 18765 -t  "cd /home/trusting/public_html/%currentfolder%; bash"
