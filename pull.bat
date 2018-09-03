3151e4ec85715a0ed076237d93f85dc995af9ab4
Velko Nikolov <velko.nikolov@gmail.com>
2018-09-03 22:00:46 +0300

    resume

--- pull.bat
+++ pull.bat
@@ -1,4 +1,4 @@
-@for %%a in (.) do set @currentfolder=%%~na
+@for %%a in (.) do @set currentfolder=%%~na
 
 :: explicit user.name etc necessary because HOME is changed and git can't find its .gitconfig
 @call ssh-temp %currentfolder% git -c "user.name=Velko Nikolov" -c "user.email=velko.nikolov@gmail.com" pull ssh://trusting@ams7.siteground.eu:18765/home/trusting/public_html/%currentfolder%
