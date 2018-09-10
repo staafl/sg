d96c422cb730b42a45fc8ec081e98f2da454ca03
Velko Nikolov <velko.nikolov@gmail.com>
2018-09-10 21:32:08 +0300

    lh.bat

--- lh.bat
+++ lh.bat
@@ -5,4 +5,4 @@
 :: OBSOLETE
 ::@start "" chrome --incognito "http://localhost:3333/%1"
 ::@start %~d0\\%~p0\\%1
-::start "" "https://htmlpreview.github.io/?https://github.com/staafl/threejs-demos/blob/master/%1/index.html"dsf
+::start "" "https://htmlpreview.github.io/?https://github.com/staafl/threejs-demos/blob/master/%1/index.html"
